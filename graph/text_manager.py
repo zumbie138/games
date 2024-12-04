52f6818fcf6106635c11e74d4819f62f882c6ec6
zumbie138 <gabriel.mks@gmail.com>
2024-12-04 10:18:27 -0300

    commit diario 04122024

---
+++ graph/text_manager.py
@@ -0,0 +1,20 @@
+from .game_texts import EnText
+
+class LanguageManager:
+    _current_language = EnText
+
+    @classmethod
+    def set_language(cls, language_class):
+        cls._current_language = language_class
+
+    @classmethod
+    def get_text(cls, key, **kwargs):
+        """
+        :param key: Chave do texto no idioma ativo.
+        :param kwargs: Variáveis para substituir no texto.
+        """
+        template = getattr(cls._current_language, key, f"Text '{key}' not found")
+        return template.format(**kwargs)
+    
+    def print_text(self, key, **kwargs):
+        print(self.get_text(key, **kwargs))
